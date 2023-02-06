fdf61bae7bac2a6562c0b11004e50917c648bf97
misskatiemarple <misskatiemarple@gmail.com>
2023-02-06 13:54:49 +0900

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "536bc94baaa1ce2ffb3ebf0f95098d9a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
