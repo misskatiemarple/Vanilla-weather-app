7ecb91f133a2d3caae83ea7a795ad27135df75fb
misskatiemarple <misskatiemarple@gmail.com>
2023-02-12 13:29:44 +0900

    Injected HTML forecast string into Javascript

--- src/app.js
+++ src/app.js
@@ -23,6 +23,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHTML = `<div class ="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="#" alt ="#"" width ="36"/>
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18°C </span>
+                  <span class="weather-forecast-temperature-min">12°C</span>
+                </div>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -84,3 +106,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 search("New York");
+
+displayForecast();
